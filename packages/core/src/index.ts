d7790b400529d2e8fcc664d11a6c269c2eca297b
krouw <felipe.andrez@live.cl>
2023-02-25 15:29:18 -0300

    Update index to remove insecure data export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,3 @@
 export { CLCountry, CLRegion, CLProvince, CLCommune } from "./base-types";
 export { RegionId, ProvinceId, CommuneId } from "./generate-types";
-export { _clLocationData as data } from "./generate-data";
 export * from "./lib";
