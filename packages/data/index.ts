926b21469e3b7c148011507c18256976bef4f725
krouw <felipe.andrez@live.cl>
2025-01-19 05:25:47 +0000

    refactor: update data imports to include object and array JSON files

--- packages/data/index.ts
+++ packages/data/index.ts
@@ -1,3 +1,7 @@
-import data from "./json/data.json" with { type: "json" };
+import object from "./json/object.json" with { type: "json" };
+import array from "./json/array.json" with { type: "json" };
 
-export default data;
+export { 
+    object,
+    array
+};
