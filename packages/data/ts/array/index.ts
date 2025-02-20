695ee1ec19126f4dcfdc293f3bbcbf135b96f80e
krouw <felipe.andrez@live.cl>
2025-02-20 02:04:44 -0300

    refactor(data): update TypeScript types and restructure data imports for regions, provinces, and communes

--- packages/data/ts/array/index.ts
+++ packages/data/ts/array/index.ts
@@ -1,7 +1,27 @@
-import regionsData from "../../json/regions.json" with { type: "json" };
-import provincesData from "../../json/provinces.json" with { type: "json" };
 import communesData from "../../json/communes.json" with { type: "json" };
+import provincesData from "../../json/provinces.json" with { type: "json" };
+import regionsData from "../../json/regions.json" with { type: "json" };
 
-export const regions = regionsData
-export const provinces = provincesData
-export const communes = communesData
+export type CLCommuneItem = {
+	id: string;
+	name: string;
+	provinceId: string;
+};
+
+export type CLProvinceItem = {
+	id: string;
+	name: string;
+	regionId: string;
+};
+
+export type CLRegionItem = {
+	id: string;
+	name: string;
+	shortName: string;
+	abbreviation: string;
+	isoCode: string;
+};
+
+export const regions: CLRegionItem[] = regionsData;
+export const provinces: CLProvinceItem[] = provincesData;
+export const communes: CLCommuneItem[] = communesData;
