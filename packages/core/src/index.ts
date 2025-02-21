1de255bccf12e32d96bc403ab90a12025fdbf9f1
krouw <felipe.andrez@live.cl>
2025-02-21 13:06:07 -0300

    refactor: update type generation script and migrate to ES module format

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,1 @@
-export { CLCountry, CLRegion, CLProvince, CLCommune } from "./base-types";
-export { RegionId, ProvinceId, CommuneId } from "./generate-types";
 export * from "./lib";
--- packages/core/src/lib.ts
+++ packages/core/src/lib.ts
@@ -1,138 +1,145 @@
-import { CLRegion, CLProvince, CLCommune } from "./base-types";
-import { RegionId, ProvinceId, CommuneId } from "./generate-types";
-import { _clLocationData } from "./generate-data";
+import type {
+	CLRegionObject,
+	CLProvinceObject,
+	CLCommuneObject,
+} from "@clregions/data/object";
+import { clRegions } from "@clregions/data/object";
+import type { RegionId, ProvinceId, CommuneId } from "./generate-types";
 
 /**
  * This comment _supports_ [Markdown](https://marked.js.org/)
  */
-export const getAllRegions = (): Record<string, CLRegion> =>
-  JSON.parse(JSON.stringify(_clLocationData.regions));
+export const getAllRegions = (): Record<string, CLRegionObject> =>
+	JSON.parse(JSON.stringify(clRegions.regions));
 
-export const getRegionById = (regionId: RegionId): CLRegion | null =>
-  _clLocationData.regions[regionId]
-    ? JSON.parse(JSON.stringify(_clLocationData.regions[regionId]))
-    : null;
+export const getRegionById = (regionId: RegionId): CLRegionObject | null =>
+	clRegions.regions[regionId]
+		? JSON.parse(JSON.stringify(clRegions.regions[regionId]))
+		: null;
 
 export const getAllProvincesByRegion = (
-  regionId: RegionId
-): Record<string, CLProvince> | null => {
-  if (!_clLocationData.regions[regionId as RegionId]) {
-    return null;
-  }
+	regionId: RegionId,
+): Record<string, CLProvinceObject> | null => {
+	if (!clRegions.regions[regionId as RegionId]) {
+		return null;
+	}
 
-  return Object.entries(
-    JSON.parse(
-      JSON.stringify(_clLocationData.regions[regionId as RegionId].provinces)
-    )
-  ).reduce(
-    (acc, [key, value]) => ({
-      ...acc,
-      [key]: value,
-    }),
-    {}
-  );
+	return Object.entries(
+		JSON.parse(
+			JSON.stringify(clRegions.regions[regionId as RegionId].provinces),
+		),
+	).reduce(
+		(acc, [key, value]) => ({
+			...acc,
+			[key]: value,
+		}),
+		{},
+	);
 };
 
 export const getAllCommunesByRegion = (
-  regionId: RegionId
-): Record<string, CLCommune> | null => {
-  if (!_clLocationData.regions[regionId as RegionId]) {
-    return null;
-  }
+	regionId: RegionId,
+): Record<string, CLCommuneObject> | null => {
+	if (!clRegions.regions[regionId as RegionId]) {
+		return null;
+	}
 
-  return Object.values(
-    JSON.parse(
-      JSON.stringify(_clLocationData.regions[regionId].provinces)
-    ) as Record<string, CLProvince>
-  )
-    .map((province) => Object.entries(province.communes))
-    .flat()
-    .reduce(
-      (acc, [key, value]) => ({
-        ...acc,
-        [key]: value,
-      }),
-      {}
-    );
+	return Object.values(
+		JSON.parse(JSON.stringify(clRegions.regions[regionId].provinces)) as Record<
+			string,
+			CLProvinceObject
+		>,
+	)
+		.map((province) => Object.entries(province.communes))
+		.flat()
+		.reduce(
+			(acc, [key, value]) => ({
+				...acc,
+				[key]: value,
+			}),
+			{},
+		);
 };
 
-export const getAllProvinces = (): Record<string, CLProvince> =>
-  Object.values(_clLocationData.regions)
-    .map((region) =>
-      Object.entries(JSON.parse(JSON.stringify(region.provinces)))
-    )
-    .flat()
-    .reduce(
-      (acc, [key, value]) => ({
-        ...acc,
-        [key]: value,
-      }),
-      {}
-    );
+export const getAllProvinces = (): Record<string, CLProvinceObject> =>
+	Object.values(clRegions.regions)
+		.map((region) =>
+			Object.entries(JSON.parse(JSON.stringify(region.provinces))),
+		)
+		.flat()
+		.reduce(
+			(acc, [key, value]) => ({
+				...acc,
+				[key]: value,
+			}),
+			{},
+		);
 
-export const getProvinceById = (provinceId: ProvinceId): CLProvince | null => {
-  try {
-    return (
-      JSON.parse(
-        JSON.stringify(
-          _clLocationData.regions[
-            provinceId.length === 2
-              ? provinceId.slice(0, 1)
-              : provinceId.slice(0, 2)
-          ].provinces[provinceId]
-        )
-      ) || null
-    );
-  } catch {
-    return null;
-  }
+export const getProvinceById = (
+	provinceId: ProvinceId,
+): CLProvinceObject | null => {
+	try {
+		return (
+			JSON.parse(
+				JSON.stringify(
+					clRegions.regions[
+						provinceId.length === 2
+							? provinceId.slice(0, 1)
+							: provinceId.slice(0, 2)
+					].provinces[provinceId],
+				),
+			) || null
+		);
+	} catch {
+		return null;
+	}
 };
 
 export const getAllCommunesByProvince = (
-  provinceId: ProvinceId
-): Record<string, CLCommune> | null => {
-  try {
-    return Object.entries(
-      JSON.parse(
-        JSON.stringify(
-          _clLocationData.regions[provinceId.slice(0, 2)].provinces[provinceId]
-            .communes
-        )
-      )
-    ).reduce(
-      (acc, [key, value]) => ({
-        ...acc,
-        [key]: value,
-      }),
-      {}
-    );
-  } catch {
-    return null;
-  }
+	provinceId: ProvinceId,
+): Record<string, CLCommuneObject> | null => {
+	try {
+		return Object.entries(
+			JSON.parse(
+				JSON.stringify(
+					clRegions.regions[provinceId.slice(0, 2)].provinces[provinceId]
+						.communes,
+				),
+			),
+		).reduce(
+			(acc, [key, value]) => ({
+				...acc,
+				[key]: value,
+			}),
+			{},
+		);
+	} catch {
+		return null;
+	}
 };
 
-export const getAllCommunes = (): Record<string, CLCommune> =>
-  Object.values(_clLocationData.regions)
-    .map((region) => Object.values(region.provinces))
-    .flat()
-    .map((province) => Object.entries(province.communes))
-    .flat()
-    .reduce(
-      (acc, [key, value]) => ({
-        ...acc,
-        [key]: value,
-      }),
-      {}
-    );
+export const getAllCommunes = (): Record<string, CLCommuneObject> =>
+	Object.values(clRegions.regions)
+		.flatMap((region) => Object.values(region.provinces))
+		.flatMap((province) => Object.entries(province.communes))
+		.reduce(
+			(acc, [key, value]) => ({
+				...acc,
+				[key]: value,
+			}),
+			{},
+		);
 
-export const getCommuneById = (communeId: CommuneId): CLCommune | null => {
-  try {
-    return {
-      ..._clLocationData.regions[communeId.slice(0, 2)].provinces[
-        communeId.slice(0, 3)
-      ].communes[communeId],
-    };
-  } catch {
-    return null;
-  }
+export const getCommuneById = (
+	communeId: CommuneId,
+): CLCommuneObject | null => {
+	try {
+		return {
+			...clRegions.regions[communeId.slice(0, 2)].provinces[
+				communeId.slice(0, 3)
+			].communes[communeId],
+		};
+	} catch {
+		return null;
+	}
 };
